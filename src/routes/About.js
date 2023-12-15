b523f78e202ce97347ee4f63badacb40d9504668
Kerem Şahin <kereemsahin@icloud.com>
2023-12-15 15:14:08 +0300

    first commit

---
+++ src/routes/About.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import NavBar from "../components/NavBar"
+
+function About() {
+  return (
+    <>
+        <NavBar/>
+    </>
+  )
+}
+export default About
---
+++ src/routes/Service.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import NavBar from "../components/NavBar"
+function Service() {
+  return (
+    <>
+      <NavBar/>
+    </>
+  )
+}
+export default Service
