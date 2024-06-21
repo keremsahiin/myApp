867edb8c1bb7d5393390200ca115f30bfcaa7896
Kerem Şahin <kereemsahin@icloud.com>
2024-06-21 21:02:00 +0300

    template complated

--- src/routes/Result.js
+++ src/routes/Result.js
@@ -50,13 +50,11 @@ function Result() {
     };
 
     return (
-        <body>
-            <div className="container">
-                <h1>Result Page</h1>
-                <button className="button" onClick={goToHome}>Go to Homepage</button>
-                {data ? renderData(data) : <p>No data available</p>}
-            </div>
-        </body>
+        <div className="result">
+            <h1>Result Page</h1>
+            <button className="button" onClick={goToHome}>Go to Homepage</button>
+            {data ? renderData(data) : <p>No data available</p>}
+        </div>
     );
 }
 
